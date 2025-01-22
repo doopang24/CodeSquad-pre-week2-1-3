6e67b6e41bc2d16076789c11621b5e297de6448c
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-22 15:05:06 +0900

    feat: main()에 미로 2차원 배열 입력

--- Maze.java
+++ Maze.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Pos {
     int x;
     int y;
@@ -7,10 +9,27 @@ class Pos {
         this.y = y;
     }
 }
+
 public class Maze {
+
     public void DFS(int[][] myMaze) {
         int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
         ArrayList<Pos> path = new ArrayList<>();
 
+    }
+
+    public static void main(String[] args) {
+        Maze maze = new Maze();
+
+        int[][] myMaze = {
+                {0, 0, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 0, 1, 1, 1, 1},
+                {1, 1, 1, 0, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 0}
+        };
     }
 }
